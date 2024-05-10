fcbec4db4d1621980b93d7f05036f18838ae7899
ds0ngsta <93960625+ds0ngsta@users.noreply.github.com>
2024-05-10 16:25:34 -0700

    Implemented /documents page along with authentication

--- notion-clone/app/(marketing)/_components/heading.tsx
+++ notion-clone/app/(marketing)/_components/heading.tsx
@@ -8,38 +8,38 @@ import { ArrowBigRight, ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export const Heading = () => {
-    const {isAuthenticated, isLoading} = useConvexAuth();
-    return(
-        <div className="max-w-3xl space-y-4">
-            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-                Your Ideas, Documents, & plans. Unified. Welcome to <span className="underline">Notion</span>
-            </h1>
-            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-                Notion is the connected workspace where <br /> 
-                better, faster work happens.
-            </h3>
-            {isLoading && (
-                <div className="w-full flex items-center justify-center">
-
-                    <Spinner size="lg"/>
-                </div>
-            )}
-            {isAuthenticated && !isLoading && (
-                <Button asChild>
-                    <Link href="/documents">
-                        Enter Notion
-                        <ArrowBigRight className="h-4 w-4 ml-2" />
-                    </Link>
-                </Button>
-            )}
-            {!isAuthenticated && !isLoading && (
-                <SignInButton mode="modal">
-                    <Button>
-                        Get Notion Free
-                        <ArrowRight className="h-4 w-4 ml-2" />
-                    </Button>
-                </SignInButton>
-            )}
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & plans. Unified. Welcome to{" "}
+        <span className="underline">Notion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Notion is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
         </div>
-    )
-}
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Notion
+            <ArrowBigRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Notion Free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
+    </div>
+  );
+};
